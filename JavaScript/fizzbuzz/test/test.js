f737080901c4dea190a218f2183e5938ba78d176
ZerrickStaples <zerrick.staples@gmail.com>
2017-11-20 18:17:13 -0500

    Wrote first failing test

--- JavaScript/fizzbuzz/test/test.js
+++ JavaScript/fizzbuzz/test/test.js
@@ -1,8 +1,21 @@
 import Mocha from 'mocha';
 import { expect } from 'chai';
+import { fizzBuzz } from '../src/index';
 
 describe("Canary test", () => {
     it("True equals true", () => {
         expect(true).to.equal(true);
     })
+})
+
+describe("FizzBuzz", () => {
+
+    describe("FizzBuzz returns a number", () => {
+        
+        it("Returns input", () => {
+            expect(fizzBuzz()).to.equal(1);
+        })
+
+    })
+
 })
