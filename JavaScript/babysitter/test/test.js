b2c0223c7bc1db7a004e4697805c77840f03991d
ZerrickStaples <zerrick.staples@gmail.com>
2017-11-20 19:39:02 -0500

    Wrote first failing test for Start time to bedtime rate

--- JavaScript/babysitter/test/test.js
+++ JavaScript/babysitter/test/test.js
@@ -1,8 +1,20 @@
 import Mocha from 'mocha';
 import { expect } from 'chai';
+import { startTimeRate } from '../src/index';
 
 describe("Canary test", () => {
     it("True equals true", () => {
         expect(true).to.equal(true);
     })
+})
+
+describe("Babysitter Kata", () => {
+
+    describe("Start time to bed time", () => {
+        
+        it("Returns $12 for one hour", () => {
+            expect(startTimeRate()).to.equal(12);
+        })
+    })
+
 })
