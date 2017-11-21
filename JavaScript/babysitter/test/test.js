600817a09311352bd8142ded00cd4c1da1576d5e
ZerrickStaples <zerrick.staples@gmail.com>
2017-11-20 19:23:20 -0500

    fixing stuff

---
+++ JavaScript/babysitter/test/test.js
@@ -0,0 +1,9 @@
+import Mocha from 'mocha';
+import { expect } from 'chai';
+
+describe("Canary test", () => {
+    it("Returns true equal true", () => {
+        expect(true).to.equal(true);
+    })
+})
+
