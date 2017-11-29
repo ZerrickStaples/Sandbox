d3f5d4a58c9c88164f292831080c194f59113979
ZerrickStaples <zerrick.staples@gmail.com>
2017-11-29 08:29:58 -0500

    Wrote passing test that returns Fizz in place of 3

--- Java/fizzbuzz/src/FizzBuzz.java
+++ Java/fizzbuzz/src/FizzBuzz.java
@@ -3,6 +3,11 @@ public class FizzBuzz
 
     public String fizz(Integer inputNumber)
     {
+
+        if(inputNumber == 3)
+        {
+            return "Fizz";
+        }
         return inputNumber.toString();
     }
 
