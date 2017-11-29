3a2c927611c9f9adc9c01b3df2c7857966fd6bf7
ZerrickStaples <zerrick.staples@gmail.com>
2017-11-29 08:42:07 -0500

    Refactored code: Placed instantiation of new FizzBuzz object above all tests.

--- Java/fizzbuzz/test/FizzBuzzTest.java
+++ Java/fizzbuzz/test/FizzBuzzTest.java
@@ -4,24 +4,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzBuzzTest
 {
 
+    FizzBuzz fizzBuzz = new FizzBuzz();
+
     @Test
     public void fizzBuzzReturnsANumber()
     {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("1", fizzBuzz.fizz(1));
     }
 
     @Test
     public void fizzBuzzReturnsInputNumber()
     {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("2", fizzBuzz.fizz(2));
     }
 
     @Test
     public void returnsFizzForMultiplesOf3()
     {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("Fizz", fizzBuzz.fizz(3));
         assertEquals("Fizz", fizzBuzz.fizz(6));
     }
@@ -29,7 +28,6 @@ public class FizzBuzzTest
     @Test
     public void returnsBuzzFor5AndMultiples()
     {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("Buzz", fizzBuzz.fizz(5));
         assertEquals("Buzz", fizzBuzz.fizz(10));
     }
@@ -37,7 +35,6 @@ public class FizzBuzzTest
     @Test
     public void returnsFizzBuzzForMultiplesOf3And5()
     {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("FizzBuzz", fizzBuzz.fizz(15));
         assertEquals("FizzBuzz", fizzBuzz.fizz(30));
     }
