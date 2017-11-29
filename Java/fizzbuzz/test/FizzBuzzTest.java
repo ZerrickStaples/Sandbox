7040effffa125bd88329f54e379f498f8137ebcd
ZerrickStaples <zerrick.staples@gmail.com>
2017-11-28 22:09:35 -0500

    Wrote first test for fizz method

--- Java/fizzbuzz/test/FizzBuzzTest.java
+++ Java/fizzbuzz/test/FizzBuzzTest.java
@@ -1,4 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest
 {
+
+    @Test
+    public void fizzBuzzReturnsANumber()
+    {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("1", fizzBuzz.fizz(1));
+    }
 
 }
