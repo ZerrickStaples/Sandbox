647c16ef055ff477dc16fea3dad27df74f413b9c
ZerrickStaples <zerrick.staples@gmail.com>
2017-11-29 08:35:01 -0500

    Wrote code passing test that returns Buzz for multiples of 5

--- Java/fizzbuzz/src/FizzBuzz.java
+++ Java/fizzbuzz/src/FizzBuzz.java
@@ -4,6 +4,11 @@ public class FizzBuzz
     public String fizz(Integer inputNumber)
     {
 
+        if(inputNumber % 5 == 0)
+        {
+            return "Buzz";
+        }
+
         if(inputNumber % 3 == 0)
         {
             return "Fizz";
