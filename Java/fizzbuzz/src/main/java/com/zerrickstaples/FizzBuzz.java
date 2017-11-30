f467cf8197adc6c035655cf70c5eaaa2b7770e2f
ZerrickStaples <zerrick.staples@gmail.com>
2017-11-29 21:00:54 -0500

    Wrote code that returns 'Buzz' for inputNumbers that have no remainder when divided by five.

--- Java/fizzbuzz/src/main/java/com/zerrickstaples/FizzBuzz.java
+++ Java/fizzbuzz/src/main/java/com/zerrickstaples/FizzBuzz.java
@@ -3,7 +3,7 @@ package com.zerrickstaples;
 public class FizzBuzz {
     public static String calc(Integer inputNumber) {
 
-        if (inputNumber == 5){
+        if (inputNumber % 5 == 0){
             return "Buzz";
         }
         if(inputNumber % 3 == 0){
