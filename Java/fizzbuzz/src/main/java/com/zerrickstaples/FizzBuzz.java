258eee0aab1c1eff3bcb72c6a6bbbb4d091011c1
ZerrickStaples <zerrick.staples@gmail.com>
2017-11-29 21:12:34 -0500

    Refactored.

--- Java/fizzbuzz/src/main/java/com/zerrickstaples/FizzBuzz.java
+++ Java/fizzbuzz/src/main/java/com/zerrickstaples/FizzBuzz.java
@@ -3,15 +3,27 @@ package com.zerrickstaples;
 public class FizzBuzz {
     public static String calc(Integer inputNumber) {
 
-        if(inputNumber % 15 == 0){
+        if(isFizzBuzz(inputNumber)){
             return "FizzBuzz";
         }
-        if (inputNumber % 5 == 0){
+        if (isBuzz(inputNumber)){
             return "Buzz";
         }
-        if(inputNumber % 3 == 0){
+        if(isFizz(inputNumber)){
             return "Fizz";
         }
         return inputNumber.toString();
+    }
+
+    private static boolean isFizz(Integer inputNumber) {
+        return inputNumber % 3 == 0;
+    }
+
+    private static boolean isBuzz(Integer inputNumber) {
+        return inputNumber % 5 == 0;
+    }
+
+    private static boolean isFizzBuzz(Integer inputNumber) {
+        return inputNumber % 15 == 0;
     }
 }
